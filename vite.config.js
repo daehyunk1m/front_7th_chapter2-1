a2f1875e7f958c7038569570ec7350bd188645d0
daehyun kim <101967272+daehyunk1m@users.noreply.github.com>
2025-11-14 04:31:39 +0900

    Fix: 빌드 세팅 변경

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vitest/config";
 
-export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/front_7th_chapter2-1/" : "/", // 프로덕션 빌드시에만 GitHub Pages base 경로 적용
+export default defineConfig(({ mode }) => ({
+  // base: command === "build" ? "/front_7th_chapter2-1/" : "/", // 프로덕션 빌드시에만 GitHub Pages base 경로 적용
+  base: mode === "production" ? "/front_7th_chapter2-1/" : "/", // 프로덕션 빌드시에만 GitHub Pages base 경로 적용
   test: {
     globals: true,
     environment: "jsdom",
